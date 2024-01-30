99cc49896e9da92106e14e6905ce1072ed7269fb
Getonto3 <getontomoses18@gmail.com>
2024-01-30 16:12:49 +0300

    feat: setting up project layout

--- appointment/src/Layout.jsx
+++ appointment/src/Layout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Flex } from "@chakra-ui/react";
 
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer";
 export default function Layout() {
   return (
     <Flex minH="100vh" direction={"column"} justify={"space-between"}>
-      {/* direction="column" align="center" maxW={{ xl: "1200px" }} m="0 auto" */}
+      <Navbar />
       <Outlet />
+      <Footer />
     </Flex>
   );
 }
