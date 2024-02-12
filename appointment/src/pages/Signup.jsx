cc442f61908b52b41712e1798f0226904322058a
Getonto3 <getontomoses18@gmail.com>
2024-02-12 19:56:42 +0300

    feat: the signup form

--- appointment/src/pages/Signup.jsx
+++ appointment/src/pages/Signup.jsx
@@ -1,5 +1,131 @@
+import { useState } from "react";
+import {
+  useToast,
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
 export default function Signup() {
+  const toast = useToast();
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { username, email, password } = values;
+    try {
+      const res = await fetch("http://127.0.0.1:8000/items/user-register/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+      });
+      console.log(res);
+      if (res.status !== 201) {
+        toast({
+          title: "Error Occured.",
+          description: "Something Went Wrong Please Try Again.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "Account created.",
+          description: "We've created your account for you.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } catch (error) {
+      toast({
+        title: "Error Occured.",
+        description: "Unable to contact server",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  }
   return (
-    <h1>About</h1>
-  )
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Create account</Heading>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="name">
+              <FormLabel>Username</FormLabel>
+
+              <Input onChange={handleChange} name="username" type="text" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input onChange={handleChange} name="email" type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input onChange={handleChange} name="password" type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Remember me</Checkbox>
+                <Text color={"blue.400"}>Forgot password?</Text>
+              </Stack>
+              <Button
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+                onClick={handleSubmit}
+              >
+                Submit
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
